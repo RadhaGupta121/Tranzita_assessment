4f8b9eb39a2af58184606f9940813f4eaaf72308
Radha gupta <1radha21@gmail.com>
2023-10-07 09:53:44 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import 'font-awesome/css/font-awesome.min.css';
+import Main from './Component/Main';
+import MyNavbar from './Component/MyNavbar';
+function App() {
+  return (
+    <>
+    <MyNavbar/>
+    <Main/>
+    </>
+    
+  );
+}
+
+export default App;
